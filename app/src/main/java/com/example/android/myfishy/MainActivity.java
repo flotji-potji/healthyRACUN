82a92cbac72ccd8aa9ae6a655b9415f2532ff78e
CookieMaxx <cookiemax.lp@gmail.com>
2021-04-24 16:11:10 +0200

    MPAndroidChart Libary hinzugefügt Piechart für activity_main.xml hinzugefügt activity_nutrition.xml hinzugefügt  -> soll beim anklicken der ersten viewcard geöffnet werden und detailierte Infos ausgeben soll
     Geplant: activity_nutrition.xml soll ein Wochenrückblick haben, wo von einem bestimmten Tag die Informationen
     angegeben werden sollen
    
     activity_main.xml soll noch mehrere viewcards funktionen bekommen wie eine BMI ViewCard, Meal ViewCard etc.
     (man muss überlegen was diese genau machen sollen bzw. wo es sich lohnt eine eigene card zu machen)

--- app/src/main/java/com/example/android/myfishy/MainActivity.java
+++ app/src/main/java/com/example/android/myfishy/MainActivity.java
@@ -1,13 +1,81 @@
 package com.example.android.myfishy;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private PieChart pieChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pieChart = findViewById(R.id.piechart);
+        setupPieChart();
+        loadPieChart();
     }
+
+    private void setupPieChart(){
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setUsePercentValues(true);
+        pieChart.setEntryLabelTextSize(12);
+        pieChart.setEntryLabelColor(Color.BLACK);
+//        pieChart.setCenterText("Data");
+//        pieChart.setCenterTextSize(24);
+        pieChart.getDescription().setEnabled(false);
+
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setEnabled(true);
+    }
+    private void loadPieChart(){
+
+        ArrayList<PieEntry> entries = new ArrayList<>();
+
+
+        //value: daten die mitgegeben werden, um anzuzeigen wv der nährstoffe eingenommen wurdem
+        entries.add(new PieEntry(0.4f, "Potassium"));
+        entries.add(new PieEntry(0.3f, "Iron"));
+        entries.add(new PieEntry(0.1f, "Kalium"));
+        entries.add(new PieEntry(0.2f, "Calcium"));
+
+        //colors liste wurde erstellt um die entries in farben einzukategorisieren.
+        ArrayList<Integer> colors = new ArrayList<>();
+        for(int color: ColorTemplate.MATERIAL_COLORS)
+        {
+            colors.add(color);
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, "Nutritions:");
+        dataSet.setColors(colors);
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(pieChart));
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.BLACK);
+
+        pieChart.setData(data);
+        pieChart.invalidate();
+        pieChart.animateY(600, Easing.EaseInOutQuad);
+
+    }
+
+
 }
