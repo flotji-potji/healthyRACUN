c5259d58a1c1345284b81638af578f38880d8f6c
Florian Schmidt <nme199911@gmail.com>
2021-04-29 20:00:44 +0200

    User Klasse mit TODOS zu Erstellung einer DB-Entity erstellt.

---
+++ app/src/main/java/com/example/android/myfishy/db/entities/User.java
@@ -0,0 +1,11 @@
+package com.example.android.myfishy.db.entities;
+
+//TODO: richtigen Identifier hinzufügen, um als Tabelle kenn zu zeichnen
+public class User {
+
+    //TODO: richtige Attribute aus ER-Diagramm hinzufügen. Attribute sollen private sein und mit richtigen Identifiern versehen werden.
+
+    //TODO: Konstruktor mit erstellten Attributen erzeugen
+
+    //TODO: getter-Methoden aus erstellten Attributen erzeugen
+}
