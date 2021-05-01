61c52e3b87a6966f8ab8788cad1f2d704359670a
Florian Schmidt <nme199911@gmail.com>
2021-05-01 20:09:06 +0200

    added queries in order to join tables and get their shared entries

--- app/src/main/java/com/example/android/myfishy/db/HealthyDao.java
+++ app/src/main/java/com/example/android/myfishy/db/HealthyDao.java
@@ -1,30 +1,43 @@
 package com.example.android.myfishy.db;
 
-import androidx.room.Dao;
-import androidx.room.Insert;
+import androidx.room.*;
 import com.example.android.myfishy.db.entities.*;
+import com.example.android.myfishy.db.relations.MealConsistsOfNourishments;
+import com.example.android.myfishy.db.relations.UserEatMeals;
+import com.example.android.myfishy.db.relations.UserGotDiets;
+
+import java.util.List;
 
 @Dao
 public interface HealthyDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insertDiet(Diet diet);
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertDietaryRestrictionTable(DietaryRestrictionTable dietaryRestrictionTable);
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insertMeal(Meal meal);
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insertNourishment(Nourishment nourishment);
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertNutritionFactTable(NutritionFactTable nutritionFactTable);
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insertUser(User user);
 
+    @Transaction
+    @Query("SELECT * FROM meal WHERE meal_id = :mealId")
+    List<MealConsistsOfNourishments> getMealConsistsOfNourishments(int mealId);
 
+    @Transaction
+    @Query("SELECT * FROM user WHERE username = :username")
+    List<UserEatMeals> getUserEatMeals(String username);
 
+    @Transaction
+    @Query("SELECT * FROM user WHERE username = :username")
+    List<UserGotDiets> getUserGotDiets(String username);
 }
