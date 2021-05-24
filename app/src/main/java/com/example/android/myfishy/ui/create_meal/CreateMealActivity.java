c7cdb6b73a1bb4c33044114853217241670b6384
Florian Schmidt <nme199911@gmail.com>
2021-05-24 23:22:59 +0200

    elevated create meal to an activity to cope with main activity

---
+++ app/src/main/java/com/example/android/myfishy/ui/create_meal/CreateMealActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.myfishy.ui.create_meal;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.android.myfishy.R;
+
+public class CreateMealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
