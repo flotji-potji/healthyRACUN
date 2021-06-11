4e285f89b545c9e0ffc506d596a8d67c2ea44c17
MarveloursMrPBH <be20b018@technikum-wien.at>
2021-06-11 09:29:48 +0200

    re-introduced mock-up code into home fragment and view model (pie chart not functional yet);  [It's like evolution, just backwards]

--- app/src/main/java/com/example/android/myfishy/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/android/myfishy/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.myfishy.ui.home;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +12,76 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import com.example.android.myfishy.R;
+import com.github.mikephil.charting.charts.PieChart;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        final View root = inflater.inflate(R.layout.content_dashboard, container, false);
+
+        final TextView tvPotassium = root.findViewById(R.id.tvPotassium);
+        final TextView tvSodium = root.findViewById(R.id.tvSodium);
+        final TextView tvPhosphate = root.findViewById(R.id.tvPhosphate);
+        final TextView tvCalcium = root.findViewById(R.id.tvCalcium);
+        final TextView tvWater = root.findViewById(R.id.tvWater);
+        final PieChart pieChart = root.findViewById(R.id.piechart);
+
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<List<String>>() {
+            @Override
+            public void onChanged(@Nullable List<String> ml) {
+                assert ml != null;
+                tvPotassium.setText(ml.get(0));
+                tvSodium.setText(ml.get(1));
+                tvPhosphate.setText(ml.get(2));
+                tvCalcium.setText(ml.get(3));
+                tvWater.setText(ml.get(4));
+
+                /*
+                pieChart.addPieSlice(
+                        new PieModel(
+                                getString(R.string.potassium_label),
+                                Integer.parseInt(tvPotassium.getText().toString()),
+                                Color.parseColor("#" + Integer.toHexString(getContext().getColor(R.color.potassium)))));
+                pieChart.addPieSlice(
+                        new PieModel(
+                                getString(R.string.sodium_label),
+                                Integer.parseInt(tvSodium.getText().toString()),
+                                Color.parseColor("#" + Integer.toHexString(getContext().getColor(R.color.sodium)))));
+                pieChart.addPieSlice(
+                        new PieModel(
+                                getString(R.string.phosphate_label),
+                                Integer.parseInt(tvPhosphate.getText().toString()),
+                                Color.parseColor("#" + Integer.toHexString(getContext().getColor(R.color.phosphate)))));
+                pieChart.addPieSlice(
+                        new PieModel(
+                                getString(R.string.calcium_label),
+                                Integer.parseInt(tvCalcium.getText().toString()),
+                                Color.parseColor("#" + Integer.toHexString(getContext().getColor(R.color.calcium)))));
+                pieChart.addPieSlice(
+                        new PieModel(
+                                getString(R.string.water_label),
+                                Integer.parseInt(tvWater.getText().toString()),
+                                Color.parseColor("#" + Integer.toHexString(getContext().getColor(R.color.water)))
+                        )
+                );
+                */
+                // To animate the pie chart
+                pieChart.startAnimation();
+            }
+        });
+
+        return root;
+    }
+}
+/*      Commented out Placeholder-Code
 
     private HomeViewModel homeViewModel;
 
@@ -30,4 +99,4 @@ public class HomeFragment extends Fragment {
         });
         return root;
     }
-}
+ */
--- app/src/main/java/com/example/android/myfishy/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/android/myfishy/ui/home/HomeViewModel.java
@@ -4,9 +4,31 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<List<String>> mList;
+
+    public HomeViewModel() {
+        mList = new MutableLiveData<>();
+        List<String> ml = new ArrayList<String>();
+        ml.add(Integer.toString(7));
+        ml.add(Integer.toString(25));
+        ml.add(Integer.toString(15));
+        ml.add(Integer.toString(3));
+        ml.add(Integer.toString(50));
+        mList.setValue(ml);
+    }
+
+    public LiveData<List<String>> getText() {
+        return mList;
+    }
+}
+
+/*      Commented out Placeholder-Code
+        private MutableLiveData<String> mText;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
@@ -16,4 +38,5 @@ public class HomeViewModel extends ViewModel {
     public LiveData<String> getText() {
         return mText;
     }
-}
+
+ */
