161e1ce15b2ea35337297d48bce5bff56e053ce8
CookieMaxx <cookiemax.lp@gmail.com>
2021-06-12 22:11:03 +0200

    New logo added

--- app/src/main/java/com/example/android/myfishy/ui/nutrition_alarm/NutritionAlarmFragment.java
+++ app/src/main/java/com/example/android/myfishy/ui/nutrition_alarm/NutritionAlarmFragment.java
@@ -21,13 +21,13 @@ public class NutritionAlarmFragment extends Fragment {
         notificationsViewModel =
                 new ViewModelProvider(this).get(NutritionAlarmViewModel.class);
         View root = inflater.inflate(R.layout.fragment_nutrition_alarm, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_notifications);
+//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
